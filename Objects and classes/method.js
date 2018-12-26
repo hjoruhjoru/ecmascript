4f519b60832ac562bb53a2678f6b3019206e7da5
hjoruhjoru <wy24680@yahoo.com.tw>
2018-12-26 23:32:31 +0800

    Create method.js

---
+++ Objects and classes/method.js
@@ -0,0 +1,26 @@
+var People = function (name, gender) {
+  this.name = name;
+  this.gender = gender;
+  this.title = getTitle(gender);
+  
+  //public method
+  this.isMale = function () {
+    if (this.gender == 'male') {
+      return true;
+    }
+    return false;
+  }
+  
+  //private method
+  function getTitle(gender) {
+    if (gender == 'male') {
+      return 'Mr.';
+    }
+    return 'Ms.';
+  }
+}
+
+
+var Tom = new People('Tom', 'male');
+console.log(Tom.isMale()); //true
+console.log(Tom.title); //Mr.
