5336a7221ddf8bf3068ad919127ca92cd8cdb9a1
hjoruhjoru <wy24680@yahoo.com.tw>
2018-12-26 22:29:50 +0800

    Update class.js

--- Objects and classes/class.js
+++ Objects and classes/class.js
@@ -1,10 +1,10 @@
 //people class to define object
-var people = function (name, gender) {
+var People = function (name, gender) {
   //define public property and assign value in construct
   this.name = name;
   this.gender = gender;
 }
 
 //new object
-var Tom = new people('Tom', 'boy');
-var Richard = new people('Richard', 'girl');
+var Tom = new People('Tom', 'boy');
+var Richard = new People('Richard', 'girl');
