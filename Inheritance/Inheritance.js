f8ee9e55d8ac01fdb40c2ba6de0113cf68a36f8f
hjoruhjoru <wy24680@yahoo.com.tw>
2018-12-27 22:10:09 +0800

    Update Inheritance.js

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -1,7 +1,6 @@
 var People = function (name, gender) {
   this.name = name;
   this.gender = gender;
-  this.title = getTitle(gender);
   
   this.isMale = function () {
     if (this.gender == 'male') {
@@ -9,19 +8,23 @@ var People = function (name, gender) {
     }
     return false;
   }
-  
-  function getTitle(gender) {
-    if (gender == 'male') {
+}
+
+People.prototype.getTitle = function () {
+  if (this.gender == 'male') {
       return 'Mr.';
     }
     return 'Ms.';
-  }
 }
 
-//derive property from People
 var Man = function (name) {
+  //derive property from People
   People.call(this, name, 'male');
 }
 
+//derive method from People
+Man.prototype = Object.create(People.prototype);
+
 var Tom = new Man('Tom');
 console.log(Tom.gender); //male
+console.log(Tom.getTitle()); //Mr.
