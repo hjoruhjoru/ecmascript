d204aa7e3d64f23226a5b86be5f715452221c60f
hjoruhjoru <wy24680@yahoo.com.tw>
2018-12-27 21:07:15 +0800

    Update Inheritance.js

--- Inheritance/Inheritance.js
+++ Inheritance/Inheritance.js
@@ -1,1 +1,27 @@
+var People = function (name, gender) {
+  this.name = name;
+  this.gender = gender;
+  this.title = getTitle(gender);
+  
+  this.isMale = function () {
+    if (this.gender == 'male') {
+      return true;
+    }
+    return false;
+  }
+  
+  function getTitle(gender) {
+    if (gender == 'male') {
+      return 'Mr.';
+    }
+    return 'Ms.';
+  }
+}
 
+//derive property from People
+var Man = function (name) {
+  People.call(this, name, 'male');
+}
+
+var Tom = new Man('Tom');
+console.log(Tom.gender); //male
